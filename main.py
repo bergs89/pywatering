efec8950a989a0810d711f8a5d87f60a18e3dcac
Stefano Bergamini <bergs89@gmail.com>
2021-02-12 21:10:50 +0100

    INI: initial repo filling commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import humidity_sensor
+import relay
+
+from time import time
+from gpiozero import Button
+
+
+button = Button(button_port)
+
+
+while True:
+    ports = [(1,1), (2,2), (3,3), (4,4)]
+    for port in ports:
+        relay_port, sensor_port = port
+        if humidity_sensor.get_humidity(sensor_port) < 0.1:
+            relay.activate_relay(relay_port)
+            time.sleep(1)
+    button.wait_for_press(timeout=300)
+    relay.activate_relays_in_sequence(relay_ports)
---
+++ relay.py
@@ -0,0 +1,59 @@
+import sys
+import time
+
+import gpiozero
+
+
+def set_relay(status, relay_pin):
+    relay = gpiozero.OutputDevice(relay_pin, active_high=False, initial_value=False)
+    if status:
+        print("Setting relay: ON")
+        relay.on()
+    else:
+        print("Setting relay: OFF")
+        relay.off()
+
+
+def toggle_relay(relay_pin):
+    relay = gpiozero.OutputDevice(relay_pin, active_high=False, initial_value=False)
+    print("toggling relay")
+    relay.toggle()
+
+
+def activate_relay_for_time(port_number, time):
+    set_relay(True, relay_pin)
+    time.sleep(time)
+    toggle_relay(relay_pin)
+    time.sleep(0.25)
+
+
+def activate_relays_in_sequence(relay_ports, time):
+    for relay_port in relay_ports:
+        activate_relay_for_time(relay_port, time)
+
+
+def testing_loop(relay_pin):
+    # start by turning the relay off
+    set_relay(False, relay_pin)
+    while 1:
+        # then toggle the relay every second until the app closes
+        toggle_relay()
+        # wait a second
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    relay_pin = 18
+    try:
+        testing_loop()
+    except KeyboardInterrupt:
+        # turn the relay off
+        set_relay(False, relay_pin)
+        print("\nExiting application\n")
+        # exit the application
+        sys.exit(0)
+
+
+
+
+
