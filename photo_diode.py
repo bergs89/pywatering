f6c9d82b0da66aecee18bea38f3fccbac2f7c6cf
Stefano Bergamini <bergs89@gmail.com>
2021-02-13 16:44:44 +0100

    ENH

--- photo_diode.py
+++ photo_diode.py
@@ -3,11 +3,9 @@ import time
 
 
 def get_light(middle_pin, positive_pin):
-    resistance = 330  # ohm
     i = 0
     while i < 2:
         GPIO.setmode(GPIO.BCM)
-        cap = 0.000001
         adj = 2.130620985
         resistance = 330  # ohm
         capacitor = 10  # micro faraday
@@ -18,19 +16,19 @@ def get_light(middle_pin, positive_pin):
         GPIO.setup(positive_pin, GPIO.OUT)
         GPIO.output(middle_pin, False)
         GPIO.output(positive_pin, False)
-        time.sleep(charging_time_without_photo_resistor)
+        # time.sleep(charging_time_without_photo_resistor)
         # Discharge the capacitor
         GPIO.setup(middle_pin, GPIO.IN)
-        time.sleep(charging_time_without_photo_resistor)
+        # time.sleep(charging_time_without_photo_resistor)
         GPIO.output(positive_pin, True)
         start_time = time.time()
         end_time = time.time()
         while (GPIO.input(middle_pin) == GPIO.LOW):
             end_time = time.time()
         measure_resistance = end_time - start_time
-        resistance = (measure_resistance / capacitor) * adj
+        resistance_estimation = (measure_resistance / capacitor) * adj
         i = i + 1
-    return resistance
+    return resistance_estimation
 
 
 def normalize_light(resistance):
