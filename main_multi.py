a1cb088e4475e38bfa91fa1b732a1de03563e9f1
Stefano Bergamini <bergs89@gmail.com>
2021-02-16 01:59:02 +0100

    TEST

--- main_multi.py
+++ main_multi.py
@@ -31,7 +31,7 @@ def loop_from_soil_sensors():
         relay_channels = [4, 27, 22, 23]
         for analog_signal in range(0,4):
             soil_wet = soil_moisture.get_moisture(analog_signal)
-            if soil_wet == 0:
+            if soil_wet == 1:
                 time.sleep(1)
                 pump_water(relay_channels[analog_signal])
                 time.sleep(1)
