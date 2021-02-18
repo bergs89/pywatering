b919095c385fc61f6fe84af20e76de81f2ee3579
Stefano Bergamini <bergs89@gmail.com>
2021-02-18 01:16:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ from datetime import datetime, timezone
 from datetime import timedelta
 from gpiozero import Button
 from sensors import photo_resistor, relay, soil_moisture
-from multiprocessing import Process
 from libs.light import day_or_night
 
 def loop_relays():
     relay_channels = [4, 27, 22, 23]
     for relay_channel in relay_channels:
-        # create a relay object.
-        # Triggered by the output pin going low: active_high=False.
-        # Initially off: initial_value=False
         pump_water(relay_channel, flow_time = 5)
 
 
 def pump_water(relay_channel, flow_time):
+    # create a relay object.
+    # Triggered by the output pin going low: active_high=False.
+    # Initially off: initial_value=False
     try:
         relay.main_loop(relay.relay(relay_channel), flow_time)
     except KeyboardInterrupt:
@@ -48,8 +47,10 @@ def loop_from_button(Button, timeout):
     if button_is_pressed:
         loop_relays()
 
+
 def flow_calibration(flow_time):
     return flow_time
+
 
 if __name__ == '__main__':
     timeout = 3600
@@ -57,10 +58,7 @@ if __name__ == '__main__':
         light = day_or_night(place="brussels")
         light = 1
         if light == 1:
-            soil_sensors_thread = threading.Thread(target=loop_from_soil_sensors).start()
+            soil_sensors_thread = threading.Thread(target=loop_from_soil_sensors, args=()).start()
             button_thread = threading.Thread(target=loop_from_button, args=(Button, timeout)).start()
             soil_sensors_thread.join()
             button_thread.join()
-        else:
-            time.sleep(timeout)
-
