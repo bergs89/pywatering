13dd1735bd808f14863595982dc6697ffbaace9e
Stefano Bergamini <bergs89@gmail.com>
2021-03-06 19:34:25 +0100

    Update get_env_properties.py

--- get_env_properties.py
+++ get_env_properties.py
@@ -4,10 +4,6 @@ import matplotlib.pyplot as plt
 
 from sensors.libraries import bme280
 
-temperature_list = []
-pressure_list = []
-humidity_list = []
-time_list = []
 
 def save_plot_as_pic(x, y, figname):
     plt.plot(x, y)
@@ -15,18 +11,22 @@ def save_plot_as_pic(x, y, figname):
     plt.savefig(figname)
 
 
-while True:
-    temperature, pressure, humidity = bme280.readBME280All()
-    temperature_list.append(temperature)
-    pressure_list.append(pressure)
-    humidity_list.append(humidity)
-    time_list.append(datetime.datetime.now())
-    save_plot_as_pic(time_list, temperature_list, 'data/temperature_plot.jpg')
-    save_plot_as_pic(time_list, pressure_list, 'data/pressure_plot.jpg')
-    save_plot_as_pic(time_list, humidity_list, 'data/humidity_plot.jpg')
+def store_env_data_indefinetely():
+    temperature_list = []
+    pressure_list = []
+    humidity_list = []
+    time_list = []
+    while True:
+        temperature, pressure, humidity = bme280.readBME280All()
+        temperature_list.append(temperature)
+        pressure_list.append(pressure)
+        humidity_list.append(humidity)
+        time_list.append(datetime.datetime.now())
+        save_plot_as_pic(time_list, temperature_list, 'data/temperature_plot.jpg')
+        save_plot_as_pic(time_list, pressure_list, 'data/pressure_plot.jpg')
+        save_plot_as_pic(time_list, humidity_list, 'data/humidity_plot.jpg')
+        time.sleep(1)
+        # time.sleep(3600)
 
-    time.sleep(1)
-    # time.sleep(3600)
-
-
-
+if __name__=="__main__":
+    store_env_data_indefinetely()
